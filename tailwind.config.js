b5387ec952bca527a2183463fb46e397395c59b2
Almaas Izdihar <almaasizdihar@gmail.com>
2023-10-21 07:01:14 +0700

    chore: Setup Fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        tango: ['Tango Sans', 'monospace'],
+        cygre: ['Cygre', 'serif'],
+        fira: ['Fira Code', 'serif'],
+      },
+    },
   },
   plugins: [],
 };
