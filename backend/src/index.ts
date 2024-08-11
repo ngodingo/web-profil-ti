0b04b3c7bc7fb1f4f7603a218606a7411bff14d6
Karindra Linux <namakulinux@gmail.com>
2024-08-11 14:07:23 +0700

    feat(news): save data on json file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,40 +1,46 @@
 import { Elysia, NotFoundError } from "elysia";
 
-const news = [
-  {
-    id: 1,
-    title: 'Judul Berita 1',
-    content: 'konten berita',
-    createdAt: '2024-07-21 12:00:00',
-  },
-  {
-    id: 2,
-    title: 'Judul Berita 2',
-    content: 'konten berita 2',
-    createdAt: '2024-07-20 12:00:00',
-  },
-];
 
 const app = new Elysia()
   .get("/", () => "Hello Ngodingo")
-  .get("/news", () => {
-    return news;
+  .get("/news", async () => {
+    const data = await Bun.file(`${import.meta.dir}/news.json`).json();
+    return data;
   })
-  .post("/news", ({ body }: { body: { title: string, content: string, createdAt: string } }) => {
+  .post("/news", async ({ body }: { body: { title: string, content: string } }) => {
 
-    const latestId = news[news.length - 1].id;
+    const newsLocal = await Bun.file(`${import.meta.dir}/news.json`).json();
 
-    news.push({
+    let latestId = newsLocal[0].id
+
+    for (const news of newsLocal) {
+
+      if (news.id > latestId) {
+        latestId = news.id;
+      }
+    }
+
+    const currentId = latestId + 1;
+
+    newsLocal.push({
+      id: currentId,
       ...body,
-      id: latestId + 1,
+      createdAt: new Date()
     });
 
-    return body;
+    Bun.write(`${import.meta.dir}/news.json`, JSON.stringify(newsLocal))
+
+    return {
+      id: currentId,
+      ...body,
+    };
   })
-  .get("/news/:id", ({ params }) => {
+  .get("/news/:id", async ({ params }) => {
     const { id } = params
 
-    const data = news.find((berita) => berita.id === Number(id));
+    const news = await Bun.file(`${import.meta.dir}/news.json`).json();
+
+    const data = news.find((berita: { id: number, title: string, content: string, createdAt: string }) => berita.id === Number(id));
 
     if (!data) {
       throw new NotFoundError('News Not Found');
@@ -42,37 +48,45 @@ const app = new Elysia()
 
     return data
   })
-  .put("/news/:id", ({ params, body }) => {
+  .put("/news/:id", async ({ params, body }) => {
 
     const { id } = params;
     const { title, content } = body as { title: string, content: string, createdAt: string };
 
-    const index = news.findIndex((berita) => berita.id === Number(id));
-    const currentNews = news[index];
+    const newsLocal = await Bun.file(`${import.meta.dir}/news.json`).json();
+
+    const index = newsLocal.findIndex((berita: { id: number, title: string, content: string, createdAt: string }) => berita.id === Number(id));
+    const currentNews = newsLocal[index];
 
     if (index === -1) {
       throw new NotFoundError('News Not Found');
     }
 
-    news[index] = {
+    newsLocal[index] = {
       ...currentNews,
       title,
       content
     }
 
-    return news[index];
+    Bun.write(`${import.meta.dir}/news.json`, JSON.stringify(newsLocal))
+
+    return newsLocal[index];
   })
-  .delete("/news/:id", ({ params }) => {
+  .delete("/news/:id", async ({ params }) => {
 
     const { id } = params;
 
-    const index = news.findIndex((berita) => berita.id === Number(id));
+    const newsLocal = await Bun.file(`${import.meta.dir}/news.json`).json();
+
+    const index = newsLocal.findIndex((berita: { id: number, title: string, content: string, createdAt: string }) => berita.id === Number(id));
 
     if (index === -1) {
       throw new NotFoundError('News Not Found');
     }
 
-    news.splice(index, 1);
+    newsLocal.splice(index, 1);
+
+    Bun.write(`${import.meta.dir}/news.json`, JSON.stringify(newsLocal))
 
     return "Deleted News ID : " + params.id
   })
